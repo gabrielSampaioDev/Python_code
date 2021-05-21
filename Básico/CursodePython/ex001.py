2d84799056d4eac6ba6eae88cb62092b06935692
gabrielSampaioDev <gabrielsampaio.ssa@gmail.com>
2021-05-21 09:07:06 -0300

    Renamev2

--- Básico/CursodePython/ex001.py
+++
@@ -1,2 +0,0 @@
-msg = '\033[1;32mOlá, Mundo!\033[m'
-print(msg)
--- Básico/CursodePython/ex002.py
+++
@@ -1,2 +0,0 @@
-nome = input('Qual é o seu \033[34mnome? \033[m')
-print('Olá, {}{}{}. Seja bem-vindo!!'.format('\033[1;32m', nome, '\033[m'))
